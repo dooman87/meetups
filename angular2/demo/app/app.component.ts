c34edf73b2dd58163c1d183c3ebce223edc496f6
Dmitry Pokidov <dooman87@gmail.com>
2016-02-25 20:09:03 +1100

    Reorgonised structure

--- angular2/demo/app/app.component.ts
+++ angular2/demo/app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>DEMO APP</h1>'
+    template: '<h1>My First Angular 3 App!!!!</h1>'
 })
 export class AppComponent { }
